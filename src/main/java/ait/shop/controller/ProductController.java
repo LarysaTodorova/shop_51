5b0784e9a7708297f04ad2c204a4b0aa31219a7c
Lora <lorageo33@gmail.com>
2025-09-05 18:27:13 +0300

    initial commit

--- src/main/java/ait/shop/controller/ProductController.java
+++ src/main/java/ait/shop/controller/ProductController.java
@@ -2,13 +2,22 @@ package ait.shop.controller;
 
 import ait.shop.model.entity.Product;
 import ait.shop.service.interfaces.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 // http://localhost:8080/api/products
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Product Controller", description = "Controller with operations with products")
 public class ProductController {
 
     public final ProductService service;
@@ -18,8 +27,13 @@ public class ProductController {
     }
 
     // POST /products
+    @Operation(summary = "Create product", description = "Add new product.", tags = { "Product" })
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "successful operation",
+            content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Product.class)),
+                    @Content(mediaType = "application/xml", schema = @Schema(implementation = Product.class)) }) })
+
     @PostMapping
-    public Product saveProduct(@RequestBody Product product) {
+    public Product saveProduct(@Parameter(description = "Create product object") @RequestBody Product product) {
         // обращаемся к сервису для сохранения продукта
         return service.saveProduct(product);
     }
@@ -49,6 +63,32 @@ public class ProductController {
     public Product remove(@PathVariable("productId") Long id) {
         // обращаемся к сервису для удаления
         return service.deleteProduct(id);
+    }
+
+    //DELETE -> DELETE products/by-title?title=Banana
+    @DeleteMapping("/by-title")
+    public Product removeByTitle(@RequestParam String title) {
+        return service.deleteByTitle(title);
+    }
+
+    @PutMapping("/restore/{id}")
+    public Product restoreProduct(@PathVariable Long id) {
+        return service.restoreProductById(id);
+    }
+
+    @GetMapping("/count")
+    public long getCount() {
+        return service.getProductCount();
+    }
+
+    @GetMapping("/total-price")
+    public BigDecimal getTotalPrice() {
+        return service.getTotalPrice();
+    }
+
+    @GetMapping("/average-price")
+    public BigDecimal getAveragePrice() {
+        return service.getAveragePrice();
     }
 
 }
