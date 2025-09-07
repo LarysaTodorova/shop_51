92624a6355885eef74ab212d95b2cb149034978c
Lora <lorageo33@gmail.com>
2025-09-07 23:03:12 +0300

    initial commit

---
+++ src/main/java/ait/shop/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package ait.shop.repository;
+
+
+import ait.shop.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
