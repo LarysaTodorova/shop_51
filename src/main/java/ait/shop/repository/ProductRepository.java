44114bfd51b0068ef54cc570cf670fe745ef7869
Lora <lorageo33@gmail.com>
2025-09-12 18:08:12 +0300

    initial commit

--- src/main/java/ait/shop/repository/ProductRepository.java
+++ src/main/java/ait/shop/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import ait.shop.model.dto.ProductDTO;
 import ait.shop.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // Ни одного метода внутри нет
+
+    List<Product> findByActiveTrue();
 }
---
+++ src/main/java/ait/shop/security/security_config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package ait.shop.security.security_config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/*
+1. Получение всех продуктов - всем пользователям,
+2 Получение продукта по id - аутентифицированным пользователям
+3 Сохранение продукта в БД - только администраторам (пользователя с ролью ADMIN).
+ */
+
+@EnableWebSecurity // все входящие
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/products").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products/{id}").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+
+                );
+        return http.build();
+    }
+
+}
